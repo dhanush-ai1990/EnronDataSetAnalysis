277e3650681b8cfea90be41a912799550fe6895b
Dhanush Dharmaretnam <dhanush987@gmail.com>
2017-05-28 14:31:36 -0400

    Correction for performance issues

--- Enron_Database/load_employee_table.py
+++ Enron_Database/load_employee_table.py
@@ -5,6 +5,7 @@ import sqlite3
 from datetime import datetime
 import tldextract
 import re
+from sklearn.externals import joblib
 
 
 cnx = mysql.connector.connect(user='root', password='welcome@456',
@@ -38,27 +39,41 @@ cursor.execute(SQL)
 
 
 # Connect to new Database 
-Database = sqlite3.connect('Enron_database.db')
+#Database = sqlite3.connect('Enron_database.db')
 
 
 #Create and update Employee Table
 # Note Uncomment only to modify the Employee Table.
 exception =0
 insertions =0
+emails = []
+cursor2 = cnx.cursor()
+count = 0
 for row in cursor:
+	count +=1
+	if (count%5000 ==0):
+		print "processed emails: " +str(count)
+	"""
+	if (insertions%5000==0):
+		print insertions
+		Database.commit()
+	"""
 	email_key = row[0]
 	sender = row[1]
-	emails = []
+	
 	SQL = "SELECT * FROM Enron_database.recipientinfo where mid = '%s'" %(row[0])
-	cursor2 = cnx.cursor()
+	
 	cursor2.execute(SQL)
 	emails.append(sender)
 	# Get data for TO and CC Table.
 	for record in cursor2:
 		emails.append(record[3])
 
-	emails= list(set(emails))
-	cursor2.close()
+
+	#emails= list(set(emails))
+	#cursor2.close()
+
+	"""
 	for email in emails:
 		cursor1 = cnx.cursor()
 		c = Database.cursor()
@@ -104,10 +119,17 @@ for row in cursor:
 		
 		except sqlite3.IntegrityError:
 			exception+=1
+	"""
 		
-Database.commit()
-Database.close()
+#Database.commit()
+#Database.close()
 cursor.close()
 cnx.close()
 
-print "Total number of Insertions into Employee Table is : " + str(insertions)
+print emails[1:20]
+print "Total number of emails: " +str(len(emails))
+
+emails = list(set(emails))
+print "Total number of Unique email ID: " +str(len(emails))
+joblib.dump(emails, 'UniqueEnronEmails.pkl')
+#print "Total number of Insertions into Employee Table is : " + str(insertions)
