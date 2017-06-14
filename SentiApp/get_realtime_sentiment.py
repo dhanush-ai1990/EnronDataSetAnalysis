e3dab1166d2dac51134ba9b6d55283be74dfb269
Dhanush Dharmaretnam <dhanush987@gmail.com>
2017-06-14 11:24:44 -0400

    Update for Sentiment performance

--- SentiApp/get_realtime_sentiment.py
+++ SentiApp/get_realtime_sentiment.py
@@ -1,39 +1,31 @@
 from encoder import Model
 import csv
-
+import time
 model=Model()
 def get_activations(text):
 	activations = []
+	temp2 = []
 	new_text = []
-
 	for i in range(len(text)):
 		new_text.append(text[i])
-		new_text = ''.join(new_text)
-		temp2 = []
-		temp2.append(new_text)
-		print(temp2)
-		
-		text_features = model.transform(temp2)
-		print (text_features.shape)
-		print (text_features)
+		temp_text = ''.join(new_text)
+		temp2.append(temp_text)
+	print ("Transforming")
+	print ("Number of samples to transform " + str(len(temp2)))
+	a = time.time()
+	text_features = model.transform(temp2)
+	b = time.time()
+	c = b - a
+	print ("Time taken to transform: " + str(c) + " secs")
 
-		#17.660 seconds to transform 8 examples
-		for i in range(len(temp2)):
-			sentiment = text_features[i, 2388]
-		print(temp2[i],sentiment)
+	
+	for i in range(len(temp2)):
+		sentiment = text_features[i, 2388]
 		activations.append(sentiment)
-		new_text = list(temp2)
-
 	list1 = []
 	for i in range(len(text)):
 		list1.append([text[i], activations[i]])
 
-	with open('sentiment.csv', 'w') as csvfile:
-	    fieldnames = ['new_character', 'neuron_activation']
-	    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-	    writer.writeheader()
-	    for i in range(len(text)):
-		    writer.writerow({'new_character': text[i], 'neuron_activation': activations[i]})
 
 	return list1
 
